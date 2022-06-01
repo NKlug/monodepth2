531a6f30376f6282563881d5e7c00a9ff80f4e39
Nikolas Klug <klug.nikolas@googlemail.com>
2022-06-01 22:12:10 +0200

    Add some visualization experiments

--- visualization/visualize_sequence.py
+++ visualization/visualize_sequence.py
@@ -3,12 +3,14 @@ import os
 from torch.utils.data import DataLoader
 
 import datasets
+from kitti_utils import get_absolute_camera_orientation
 from utils import readlines
 import torch
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
 import matplotlib.cm as cm
 import cv2
 import pickle
@@ -28,8 +30,8 @@ def back_project_depths(data, opt):
     cam_points = []
     with torch.no_grad():
         batch_size = opt.batch_size
-        for i in range(np.ceil(len(depths)/batch_size).astype(np.int)):
-            depths_batch = torch.from_numpy(depths[i * batch_size:(i+1) * batch_size]).cuda()
+        for i in range(np.ceil(len(depths) / batch_size).astype(np.int)):
+            depths_batch = torch.from_numpy(depths[i * batch_size:(i + 1) * batch_size]).cuda()
             inv_K_batch = torch.from_numpy(data["inv_K"][i * batch_size:(i + 1) * batch_size]).cuda()
             cam_points_batch = back_project(depths_batch, inv_K_batch).cpu().numpy()
             cam_points_batch = cam_points_batch.reshape((-1, 4, 192, 640))
@@ -43,52 +45,140 @@ def back_project_depths(data, opt):
     return data
 
 
+def get_camera_rotation_matrix(oxts):
+    """
+    Computes camera rotation matrices for leveling predicted 3d coordinates.
+    """
+    for i in range(len(oxts['lat'])):
+        pass
+
+
+def compute_3d_coordinates(predicted_depth, f=5, interim_downscale=4):
+    h, w = predicted_depth.shape[:2]
+    predicted_depth = cv2.resize(predicted_depth, (w // interim_downscale, h // interim_downscale))
+    h, w = predicted_depth.shape[:2]
+
+    aspect_ratio = h / w
+    x = np.linspace(-1, 1, w)
+    y = np.linspace(-1 * aspect_ratio, 1 * aspect_ratio, h)
+    xv, yv = np.meshgrid(x, y)
+
+    # flip y coordinates
+    yv = yv[::-1]
+    #
+    # # re-project
+    # # xv = f * xv * predicted_depth
+    # # yv = f * yv * predicted_depth
+    xv = f * xv
+    yv = f * yv
+
+    # xv = predicted_depth[0, ...]
+    # yv = predicted_depth[1, ...]
+    # zv = predicted_depth[2, ...]
+
+    zv = predicted_depth
+    image_coords = predicted_depth[:2, ...]
+
+    calib_dir = '/home/nikolas/Datasets/kitti_data/'
+
+    P = get_absolute_camera_orientation(calib_dir, None)
+    coordinates_3d = np.stack([xv.flatten(), yv.flatten(), zv.flatten()], axis=-1)
+    for i in range(len(coordinates_3d)):
+        coordinates_3d[i] = np.dot(P, np.concatenate([coordinates_3d[i], np.ones((1,))]).T).T
+
+    coordinates_3d = coordinates_3d.reshape((h, w, 3))
+    return coordinates_3d
+
+
 def visualize_sequence(data, absolute_coordinates):
+    # predicted_depths = data["cam_points"][:1]
+    predicted_depths = data["disp"][:1]
 
-    # TODO: Maybe use true camera intrinsics
-    f = 5
-    interim_downscale = 2
-    predicted_depths = data["cam_points"][:1]
+    coords = []
+    for predicted_depth in predicted_depths:
+        coords_3d = compute_3d_coordinates(predicted_depth, interim_downscale=8)
+        coords.append(coords_3d)
 
-    for predicted_depth, absolute_coordinate in zip(predicted_depths, absolute_coordinates):
-        # h, w = predicted_depth.shape[:2]
-        # predicted_depth = cv2.resize(predicted_depth, (w//interim_downscale, h//interim_downscale))
-        h, w = predicted_depth.shape[:2]
+    coords = np.asarray(coords)
 
-        # aspect_ratio = h/w
-        # x = np.linspace(-1, 1, w)
-        # y = np.linspace(-1*aspect_ratio, 1*aspect_ratio, h)
-        # xv, yv = np.meshgrid(x, y)
-        #
-        # # flip y coordinates
-        # yv = yv[::-1]
-        #
-        # # re-project
-        # # xv = f * xv * predicted_depth
-        # # yv = f * yv * predicted_depth
-        # xv = f * xv
-        # yv = f * yv
+    fig = plt.figure(figsize=(20, 10))
+    ax = fig.add_subplot(111, projection='3d', proj_type='persp')
+    # ax.set_box_aspect((1, 1, 1))
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
-        xv = predicted_depth[0, ...]
-        yv = predicted_depth[1, ...]
-        zv = predicted_depth[2, ...]
-
-        image_coords = predicted_depth[:2, ...]
-
-        # predicted_depth = 30 * predicted_depth
-
-        vmax = np.percentile(zv, 95)
-        normalizer = mpl.colors.Normalize(vmin=yv.min(), vmax=vmax)
+    for i in range(len(coords)):
+        xv = coords[i, :, :, 0]
+        yv = coords[i, :, :, 1]
+        zv = coords[i, :, :, 2]
+        pred_depth = predicted_depths[i]
+        vmax = np.percentile(pred_depth, 95)
+        normalizer = mpl.colors.Normalize(vmin=pred_depth.min(), vmax=vmax)
         mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
-        colormapped_im = mapper.to_rgba(zv)[:, :, :3]
+        h, w = pred_depth.shape[:2]
+        colormapped_im = mapper.to_rgba(cv2.resize(pred_depth, (w // 8, h // 8)))[:, :, :3]
+        scattered = ax.scatter(xv.flatten(), yv.flatten(), zv.flatten(), s=200, c=colormapped_im.reshape((-1, 3)))
+        MAX = 3
+        for direction in (-1, 1):
+            for point in np.diag(direction * MAX * np.array([1, 1, 1])):
+                ax.plot([point[0]], [point[1]], [point[2]], 'w')
+        #
+        # import plotly
+        # import plotly.graph_objs as go
+        #
+        # # Configure the trace.
+        # trace = go.Scatter3d(
+        #     x=xv.flatten(),  # <-- Put your data instead
+        #     y=yv.flatten(),  # <-- Put your data instead
+        #     z=zv.flatten(),  # <-- Put your data instead
+        #     # color_discrete_sequence=colormapped_im.reshape((-1, 3)),
+        #     mode='markers',
+        #     marker={
+        #         'size': 10,
+        #         'opacity': 0.8,
+        #     }
+        # )
+        #
+        # # Configure the layout.
+        # layout = go.Layout(
+        #     margin={'l': 0, 'r': 0, 'b': 0, 't': 0}
+        # )
+        #
+        # data = [trace]
+        #
+        # fig = go.Figure(data=data, layout=layout)
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
-        ax.scatter(xv.flatten(), yv.flatten(), zv.flatten(), s=1, c=colormapped_im.reshape((-1, 3)))
-        plt.show()
+    # def next_scatter(num, coords):
+    #     xv = coords[num, :, :, 0]
+    #     yv = coords[num, :, :, 1]
+    #     zv = coords[num, :, :, 2]
+    #     vmax = np.percentile(zv, 95)
+    #     normalizer = mpl.colors.Normalize(vmin=zv.min(), vmax=vmax)
+    #     mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+    #     colormapped_im = mapper.to_rgba(zv)[:, :, :3]
+    #     ax.clear()
+    #     scattered = ax.scatter(xv.flatten(), yv.flatten(), zv.flatten(), s=200, c=colormapped_im.reshape((-1, 3)))
+    #     # scattered = ax.plot_surface(xv, yv, zv, cmap='coolwarm', linewidth=1, antialiased=True)
+    #     MAX = 5
+    #     # for direction in (-1, 1):
+    #     #     for point in np.diag(direction * MAX * np.array([1, 1, 1])):
+    #     #         ax.plot([point[0]], [point[1]], [point[2]], 'w')
+    #
+    #     # scattered = ax.scatter(xv.flatten(), yv.flatten(), zv.flatten(), s=2)
+    #     # for line, data in zip(lines, dataLines):
+    #     #     # NOTE: there is no .set_data() for 3 dim data...
+    #     #     line.set_data(data[0:2, :num])
+    #     #     line.set_3d_properties(data[2, :num])
+    #     # scatter_plots[num-1].set_visible(False)
+    #     # scatter_plots[num].set_visible(True)
+    #
+    # line_ani = animation.FuncAnimation(fig, next_scatter, len(coords), fargs=(coords, ), interval=100, blit=False)
+
+    plt.show()
+
+
+    # fig.show()
 
     pass
 
@@ -105,6 +195,8 @@ if __name__ == '__main__':
 
     camera_coordinates = [(0, 0, 1, 0, 0, 0)]
 
-    data = back_project_depths(data, opt)
+    # data = {"disp": np.load('/home/nikolas/Projects/monodepth2/assets/test_image_disp.npy')[:, 0]}
+
+    # data = back_project_depths(data, opt)
 
     visualize_sequence(data, camera_coordinates)
