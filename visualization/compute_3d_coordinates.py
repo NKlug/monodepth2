Fix global coordinate computation

visualization/compute_3d_coordinates.py
@@ -48,13 +48,14 @@ def compute_3d_coordinates(data, downscale=1, global_coordinates=False, max_dept
 
             # compute coordinates in global coordinate system
             position = np.asarray([lat[i], lon[i], alt[i]])
-            orientation = np.asarray([roll[i], pitch[i], yaw[i] - np.pi / 2])
+            orientation = np.asarray([roll[i], pitch[i], yaw[i]])
             rot = spat.transform.Rotation.from_euler('xyz', orientation).as_matrix()
 
             global2imu = np.eye(4)
             global2imu[:3, :3] = rot
-            global2imu[3, :3] = position
+            global2imu[:3, 3] = position
             imu2global = np.linalg.inv(global2imu)
+            imu2global = global2imu
 
             h, w = coords_3d.shape[:2]
             coords_3d = np.concatenate([coords_3d, np.ones((h, w, 1))], axis=-1)

visualization/visualize_sequence.py
@@ -1,24 +1,10 @@
 import os
 
-from torch.utils.data import DataLoader
-
-import datasets
-import networks
-from kitti_utils import read_calib_file
-from trainer import Trainer
-from utils import readlines
-import torch
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.animation as animation
-import matplotlib.cm as cm
 import cv2
-import pickle
+import numpy as np
+import torch
 
 from layers import BackprojectDepth
-from options import MonodepthOptions
 
 splits_dir = os.path.join(os.path.dirname(__file__), "splits")
 
@@ -50,62 +36,11 @@ def back_project_depths(data, opt):
     return data
 
 
-def back_project_depth(predicted_depth, inv_K):
-    """
-    Compute 3d coordinates by perspective re-projection using the 2d coordinates and estimated depths.
-    """
-    # back-project as during training
-    h, w = predicted_depth.shape
-    back_project = BackprojectDepth(1, h, w).cpu()
-
-    predicted_depth = predicted_depth[np.newaxis, ...]
-    inv_K = inv_K[np.newaxis, ...]
-
-    with torch.no_grad():
-        depths_batch = torch.from_numpy(predicted_depth).cpu()
-        inv_K_batch = torch.from_numpy(inv_K).cpu()
-        cam_points = back_project(depths_batch, inv_K_batch).cpu().numpy()
-        cam_points = cam_points.reshape((-1, 4, h, w))
-        cam_points = cam_points[:, :3, ...]
-
-    return cam_points[0].T
-
-
-def get_image_to_imu_matrix(calib_dir, cam=2):
-    """Compute image to imu homogenous transformation matrix."""
-    # load calibration files
-    imu2velo = read_calib_file(os.path.join(calib_dir, 'calib_imu_to_velo.txt'))
-    cam2cam = read_calib_file(os.path.join(calib_dir, 'calib_cam_to_cam.txt'))
-    velo2cam = read_calib_file(os.path.join(calib_dir, 'calib_velo_to_cam.txt'))
-
-    # velo2cam (velo2rect) matrix
-    velo2cam = np.hstack((velo2cam['R'].reshape(3, 3), velo2cam['T'][..., np.newaxis]))
-    velo2cam = np.vstack((velo2cam, np.array([0, 0, 0, 1.0])))
-
-    # imu2velo matrix
-    imu2velo = np.hstack((imu2velo['R'].reshape(3, 3), imu2velo['T'][..., np.newaxis]))
-    imu2velo = np.vstack((imu2velo, np.array([0, 0, 0, 1.0])))
-
-    # cam2rect matrix
-    R_cam2rect = np.eye(4)
-    R_cam2rect[:3, :3] = cam2cam['R_rect_00'].reshape(3, 3)
-
-    # rect2camX matrix
-    P_rect = np.eye(4)
-    P_rect[:3, :4] = cam2cam['P_rect_0' + str(cam)].reshape(3, 4)
-
-    P_velo2im = np.dot(np.dot(P_rect, R_cam2rect), velo2cam)
-    P_imu2im = np.dot(P_velo2im, imu2velo)
-
-    # invert to get im2imu
-    return np.linalg.inv(P_imu2im)
-
-
-def experimental_compute_3d_coordinates(predicted_depth, inv_K, in_imu_coordinates=False, f=5, interim_downscale=4):
+def experimental_compute_3d_coordinates(predicted_depth, inv_K, f=5, downscale=4, *args, **kwargs):
     orig_w, orig_h = 1242, 375
 
     h, w = predicted_depth.shape[:2]
-    predicted_depth = cv2.resize(predicted_depth, (w // interim_downscale, h // interim_downscale))
+    predicted_depth = cv2.resize(predicted_depth, (w // downscale, h // downscale))
     h, w = predicted_depth.shape[:2]
     #
     x = np.linspace(-orig_w, orig_w, w)
@@ -114,7 +49,6 @@ def experimental_compute_3d_coordinates(predicted_depth, inv_K, in_imu_coordinat
 
     coords_2d = np.stack([xv, yv], axis=-1)
     coords_3d = np.concatenate([coords_2d, predicted_depth[..., np.newaxis]], axis=-1)
-
 
     # re-project
     # xv = f * xv
@@ -127,18 +61,5 @@ def experimental_compute_3d_coordinates(predicted_depth, inv_K, in_imu_coordinat
     #
     # coords_3d[:, :, 0] *= orig_w
     # coords_3d[:, :, 1] *= orig_h
-
-    if in_imu_coordinates:
-        h, w = coords_3d.shape[:2]
-        coords_3d = np.concatenate([coords_3d, np.ones((h, w, 1))], axis=-1)
-        coords_3d = coords_3d.reshape((-1, 4))
-
-        calib_dir = '/home/nikolas/Datasets/kitti_data/'
-        M = get_image_to_imu_matrix(calib_dir)
-
-        # transform to imu coords
-        coords_3d = np.dot(M, coords_3d.T).T
-
-        coords_3d = coords_3d[:, :3].reshape((h, w, 3))
 
     return coords_3d

visualization/visualizer.py
@@ -94,10 +94,11 @@ class Visualizer:
             zv = coords[on_press.num, :, :, 2]
 
             # compute colors
-            color_depths = predicted_depths[on_press.num]
+            color_depths = predicted_depths[i]
             h, w = color_depths.shape[:2]
             color_depths = cv2.resize(color_depths, (w // downscale, h // downscale))
             colors = self.compute_coloring(color_depths)
+            colors = np.swapaxes(colors, 0, 1)
 
             self.ax.set_xlabel('X')
             self.ax.set_ylabel('Y')
@@ -116,7 +117,7 @@ class Visualizer:
         """
         downscale = 8
         predicted_depths = self.data['depth']
-        coords = compute_3d_coordinates(self.data, downscale=downscale)
+        coords = compute_3d_coordinates(self.data, downscale=downscale, max_depth=3, global_coordinates=True)
 
         def next_scatter(num, coords):
             xv = coords[num, :, :, 0]
@@ -128,6 +129,7 @@ class Visualizer:
             h, w = color_depths.shape[:2]
             color_depths = cv2.resize(color_depths, (w // downscale, h // downscale))
             colors = self.compute_coloring(color_depths)
+            colors = np.swapaxes(colors, 0, 1)
 
             # plot data
             self.ax.clear()
@@ -136,11 +138,12 @@ class Visualizer:
             self.ax.set_zlabel('Z')
 
             # add bouding points for equal axes scales
-            MAX = 1
+            MAX = 2
             for direction in (-1, 1):
                 for point in np.diag(direction * MAX * np.array([1, 1, 1])):
                     self.ax.plot([point[0]], [point[1]], [point[2]], 'green')
 
+            self.plot_camera_path()
             scattered = self.ax.scatter(xv.ravel(), yv.ravel(), zv.ravel(), s=200, c=colors.reshape((-1, 3)))
 
         # create animation
@@ -165,7 +168,8 @@ class Visualizer:
         """
         downscale = 8
         predicted_depths = self.data['depth']
-        coords = compute_3d_coordinates(self.data, downscale=downscale)
+        # coords = compute_3d_coordinates(self.data, downscale=downscale, global_coordinates=False)
+        coords = compute_3d_coordinates(self.data, downscale=downscale, global_coordinates=True)
 
         xv = coords[step_num, :, :, 0]
         yv = coords[step_num, :, :, 1]